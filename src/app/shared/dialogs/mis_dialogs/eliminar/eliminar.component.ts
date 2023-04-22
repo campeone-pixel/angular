b79d11c818595bacf8421172a025cca4d8e5dd1c
matias <posesmatiasabel@gmail.com>
2023-04-22 01:19:19 -0300

    .

--- src/app/shared/dialogs/mis_dialogs/eliminar/eliminar.component.ts
+++ src/app/shared/dialogs/mis_dialogs/eliminar/eliminar.component.ts
@@ -8,18 +8,14 @@ import { AlumnosService } from '../../../../core/services/alumnos.service';
   templateUrl: './eliminar.component.html',
   styles: [],
 })
-export class EliminarComponent  {
+export class EliminarComponent {
   constructor(
     public dialogRef: MatDialogRef<EliminarComponent>,
     private alumnosService: AlumnosService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
-  
-
-
 
   eliminar() {
-    
     this.alumnosService.delete(this.data);
     this.dialogRef.close();
   }
