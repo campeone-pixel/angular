d1f717cfda7c8385b3c3cab5a7de59d4c2f7c345
matias <mapcampeone@gmail.com>
2023-05-11 06:32:17 -0300

    .

--- src/app/dashboard/pages/cursos/cursos.module.ts
+++ src/app/dashboard/pages/cursos/cursos.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CursosComponent } from './cursos.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -10,13 +9,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+
+import { CursosComponent } from './cursos.component';
 import { PipesModule } from 'src/app/pipes/pipes.module';
 import { AgregarComponent } from './abm/agregar/agregar.component';
 import { EditarComponent } from './abm/editar/editar.component';
 import { EliminarComponent } from './abm/eliminar/eliminar.component';
 import { DetalleCursoComponent } from './detalle-curso/detalle-curso.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 import { CursosRoutingModule } from './cursos-routing.module';
 
 
